c9ce95bd1790d3d24b12be44b4ad0be81627e3d4
342169159@qq.com <342169159@qq.com>
2022-12-29 15:21:33 +0800

    进一步完善

---
+++ src/components/ComponentTest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+          </nav> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PerksComponentTest",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped></style>
--- src/components/ComponentVue.vue
+++ src/components/ComponentVue.vue
@@ -1,13 +1,53 @@
 <template>
   <div>
-    <el-container>
-      <el-header>Header</el-header>
+    <el-container class="box">
+      <el-header>practice</el-header>
       <el-container>
         <el-aside width="200px">
-          <!-- <nav>
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-          </nav> -->
+          <el-row class="tac">
+            <el-col :span="24">
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+              >
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+
+                <el-menu-item index="2">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+
+                <el-menu-item index="3">
+                  <i class="el-icon-document"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+
+                <el-menu-item index="4">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航四</span>
+                </el-menu-item>
+                
+              </el-menu>
+            </el-col>
+          </el-row>
         </el-aside>
         <el-main>Main</el-main>
       </el-container>
@@ -25,13 +65,24 @@ export default {
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
 };
 </script>
 
 <style lang="less" scoped>
-.el-header,
-.el-footer {
+.box {
+  width: 100vw;
+  height: 100vh;
+}
+
+.el-header {
   background-color: #b3c0d1;
   color: #333;
   text-align: center;
@@ -50,18 +101,5 @@ export default {
   color: #333;
   text-align: center;
   line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
 }
 </style>
